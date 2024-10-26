286fa6c59abeec41c6cc4c42f5678076901e70ac
Your Name <you@example.com>
2024-10-26 12:13:19 +0200

    Deployed e2950ac with MkDocs version: 1.6.0

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var nav = document.querySelector(".md-header__inner .md-header-nav");  // Selecciona la barra de navegación
+document.getElementById('registerButton').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+});
 
-    if (nav) {
-        var button = document.createElement("a");  // Crea un enlace tipo botón
-        button.href = "https://enlace.com";  // El enlace al que debe redirigir el botón
-        button.className = "md-nav__link";  // Asigna la clase para que tenga el mismo estilo que los otros enlaces
-        button.textContent = "Botón";  // Texto del botón
-
-        // Agrega el botón al final de la barra de navegación
-        nav.appendChild(button);
-    }
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
 });
